050b0d805ebe2c4dd78be80afae7cd569c333057
Kirill.Belyachits <kirill.belyachits@effective-soft.com>
2021-12-27 18:21:59 +0300

    feat: add changes and deploy

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
 	"net/http"
 )
 
 func main() {
+	// ====== Здесь добавили лог =====
+	logrus.Info("run app")
+	// ===============================
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, fmt.Sprintf("Hello, %s", c.QueryParam("name")))
